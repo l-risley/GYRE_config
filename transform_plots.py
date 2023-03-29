8263d624f9bf2a100a00e0904036c9f085d09b92
LauraRisley <l.risley@pgr.reading.ac.uk>
2023-03-29 13:30:48 +0100

    Updated to not look at the boundaries.

--- transform_plots.py
+++ transform_plots.py
@@ -1,16 +1,15 @@
 """
 Plot the model variables, control variables, full sf and vp fields as well.
-@author: Laura Risley 2023
+@author: Laura Risley 223
 """
 import numpy as np
 
 from transforms.U_transform import *
 from transforms.T_transform import *
 from read_nemo_fields import *
-from plot_gyre_fields import *
 from general_functions import *
 
-def transform_plots(alpha, time_index):
+def transform_plots(alpha, time_index, conv):
     """
     """
     dy, dx = param['dy'], param['dx']
@@ -38,18 +37,20 @@ def transform_plots(alpha, time_index):
     eta_lon, eta_lat, time = read_file_info(eta_input_file)
     u_lon, u_lat, time = read_file_info(u_input_file)
     v_lon, v_lat, time = read_file_info(v_input_file)
+
+    # number of times in the files
     num_times = np.size(time)
 
     print(f'Length of elevation lattitude : {np.shape(eta_lat)} and longitude : {np.shape(eta_lon)}.')
     print(f'Length of u lattitude : {np.shape(u_lat)} and longitude : {np.shape(u_lon)}.')
     print(f'Length of v lattitude : {np.shape(v_lat)} and longitude : {np.shape(v_lon)}.')
 
-    ## 2d arrays for correlation analysis
     # size of eta
     ny, nx = np.shape(eta_lat)[0]-1, np.shape(eta_lon)[1]-1
     print(f'ny = {ny}, nx = {nx}')
 
-    # plot fields at both times
+    # plot full fields at both times
+
     contour(eta_lon, eta_lat, eta_0, f'at time {time[time_index]}', 'Elevation')
     contour(u_lon, u_lat, u_0, f'at time {time[time_index]}', 'Zonal Velocity')
     contour(v_lon, v_lat, v_0, f'at time {time[time_index]}', 'Meridional Velocity')
@@ -66,72 +67,101 @@ def transform_plots(alpha, time_index):
     print(f' Shape of u : {np.shape(u_0)}')
     print(f' Shape of v : {np.shape(v_0)}')
 
-    # set mask fill value
+    #######################################################
+    ##### FIND SF AND VP FOR WITHIN THE DOMAIN, NO BOUNDARY
+    eta_new = eta_0[1:-2, 2:-1]
+    u_new = u_0[1:-2, 1:-1]
+    v_new = v_0[1:-1, 2:-1]
+    ny, nx = np.shape(eta_new)
+    print(f'Shape of new elevation : {np.shape(eta_new)}')
+    print(f' Shape of new u : {np.shape(u_new)}')
+    print(f' Shape of new v : {np.shape(v_new)}')
 
-    # The fields are not the correct shape
-    eta_0, eta_diff = eta_0[:-1, 1:], eta_diff[:-1, 1:]
-    u_0, u_diff = u_0[:-1, :], u_diff[:-1, :]
-    v_0, v_diff = v_0[:, 1:], v_diff[:, 1:]
+    eta_x, eta_y = eta_lon[1:-2, 2:-1], eta_lat[1:-2, 2:-1]
+    u_x, u_y = u_lon[1:-2, 1:-1], u_lat[1:-2, 1:-1]
+    v_x, v_y = v_lon[1:-1, 2:-1], v_lat[1:-1, 2:-1]
 
-    print(f'Shape of elevation : {np.shape(eta_0)}')
-    print(f' Shape of u : {np.shape(u_0)}')
-    print(f' Shape of v : {np.shape(v_0)}')
+    contour(eta_x, eta_y, eta_new, f'no_bc', 'Elevation')
+    contour(u_x, u_y, u_new, f'no_bc', 'Zonal Velocity')
+    contour(v_x, v_y, v_new, f'no_bc', 'Meridional Velocity')
 
     # find the sf and vp for full field
-    #print(u_0)
-    #print(v_0)
-    sf_0, vp_0 = tik_reg(alpha, u_0, v_0, dy, dx, ny, nx)
+    if conv is None:
+        sf_new, vp_new = tik_reg(alpha, u_new, v_new, dy, dx, ny, nx)
+    elif conv == 'convergence':
+        sf_new, vp_new, cf_list, grad_list = tik_reg(alpha, u_new, v_new, dy, dx, ny, nx, conv)
+        # plot the convergences
+        plot_one_convergence(cf_list, 'Cost Function')
+        plot_one_convergence(grad_list, 'Gradient Norm')
 
-    # reconstruct
-    u_re, v_re = vel_from_helm(sf_0, vp_0, dx, dy)
+    # Reconstruct the velocity fields using u_transform
+    u_re, v_re = vel_from_helm(sf_new, vp_new, dx, dy)
 
-    # differences
-    du = u_0 - u_re
-    dv = v_0 - v_re
-    sf_0, vp_0 = sf_0[1:-1, 1:-1], vp_0[1:-1, 1:-1]
-    contour(eta_lon, eta_lat, sf_0, f'at time {time[time_index + 1]}', 'Streamfunction')
-    contour(eta_lon, eta_lat, vp_0, f'at time {time[time_index + 1]}', 'Velocity Potential')
+    # differences between reconstructed field and originals
+    du = u_new - u_re
+    dv = v_new - v_re
 
-    contour(u_lon, u_lat, u_re, f'recon', 'Zonal Velocity')
-    contour(v_lon, v_lat, v_re, f'recon', 'Meridional Velocity')
+    # plot sf and vp
+    sf_new, vp_new = sf_new[1:-1, 1:-1], vp_new[1:-1, 1:-1]
+    contour(eta_x, eta_y, sf_new, f'at time {time[time_index + 1]}', 'SF')
+    contour(eta_x, eta_y, vp_new, f'at time {time[time_index + 1]}', 'VP')
 
-    contour(u_lon, u_lat, du, f'recon diff', 'Zonal Velocity')
-    contour(v_lon, v_lat, dv, f'recon-diff', 'Meridional Velocity')
+    # plot reconstructed and differences
+    contour(u_x, u_y, u_re, f'Reconstructed', 'Zonal Velocity')
+    contour(v_x, v_y, v_re, f'Reconstructed', 'Meridional Velocity')
 
-    """
-    # find sf and vp for increment
-    sf_diff, vp_diff = tik_reg(alpha, u_diff, v_diff, dy, dx, ny, nx)
-    sf_diff, vp_diff = sf_diff[1:-1, 1:-1], vp_diff[1:-1, 1:-1]
-    
-    # plot both
-    
-    contour(eta_lon, eta_lat, sf_diff, f'increment', 'Streamfunction')
-    contour(eta_lon, eta_lat, vp_diff, f'increment', 'Velocity Potential')
-    
-    # find control variables
-    eta_diff, sf_u, vp_u, du_mean, dv_mean = T_transform(eta_diff, u_diff, v_diff, dx, dy, u_lat, v_lat, alpha)
-
-    # plot control variables
-    contour(eta_lon, eta_lat, sf_u[1:-1, 1:-1], f'increment', 'Unbalanced Streamfunction')
-    contour(eta_lon, eta_lat, vp_u[1:-1, 1:-1], f'increment', 'Velocity Potential')
-    
-    # recalculate the model variables
-    eta_diff, u_re, v_re = U_transform(eta_diff, sf_u, vp_u, du_mean, dv_mean, dx, dy, u_lat, v_lat)
-
-    # plot reconstructed velocities
+    contour(u_x, u_y, du, f'Reconstruction difference', 'Zonal Velocity')
+    contour(v_x, v_y, dv, f'Reconstruction difference', 'Meridional Velocity')
 
     # calculate the relative error of the fields and plot
-# add a constant to the denominator to ensure we are not dividing by zero
-    u_denom = u_diff + 1e-6
-    v_denom = v_diff + 1e-6
+    # add a constant to the denominator to ensure we are not dividing by zero
+    u_denom = u_new + 1e-6
+    v_denom = v_new + 1e-6
 
-    rel_err_u = abs((u_diff - u_re) / u_denom) * 100
-    rel_err_v = abs((v_diff - v_re) / v_denom) * 100
-    """
+    rel_err_u = abs((u_new - u_re) / u_denom) * 100
+    rel_err_v = abs((v_new - v_re) / v_denom) * 100
+
+    contour_err(u_x, u_y, rel_err_u, f'Relative Error', 'Zonal Velocity')
+    contour_err(v_x, v_y, rel_err_v, f'Relative Error', 'Meridional Velocity')
+
+    ######################################################################
+
+    ##### FIND SF AND VP FOR WITHIN THE DOMAIN, NO BOUNDARY for increments
+    deta_new = eta_diff[1:-2, 2:-1]
+    du_new = u_diff[1:-2, 1:-1]
+    dv_new = v_diff[1:-1, 2:-1]
+    ny, nx = np.shape(eta_new)
+    print(f'Shape of new elevation : {np.shape(deta_new)}')
+    print(f' Shape of new u : {np.shape(du_new)}')
+    print(f' Shape of new v : {np.shape(dv_new)}')
+
+    eta_x, eta_y = eta_lon[1:-2, 2:-1], eta_lat[1:-2, 2:-1]
+    u_x, u_y = u_lon[1:-2, 1:-1], u_lat[1:-2, 1:-1]
+    v_x, v_y = v_lon[1:-1, 2:-1], v_lat[1:-1, 2:-1]
+
+    contour(eta_x, eta_y, deta_new, f'no_bc increment', 'Elevation')
+    contour(u_x, u_y, du_new, f'no_bc increment', 'Zonal Velocity')
+    contour(v_x, v_y, dv_new, f'no_bc increment', 'Meridional Velocity')
+
+    # find the sf and vp for increment
+    if conv is None:
+        sf_new, vp_new = tik_reg(alpha, du_new, dv_new, dy, dx, ny, nx)
+    elif conv == 'convergence':
+        sf_new, vp_new, cf_list, grad_list = tik_reg(alpha, du_new, dv_new, dy, dx, ny, nx, conv)
+        # plot the convergences
+        plot_one_convergence(cf_list, 'Cost Function')
+        plot_one_convergence(grad_list, 'Gradient Norm')
+
+    dsf_new, dvp_new = sf_new[1:-1, 1:-1], vp_new[1:-1, 1:-1]
+
+    contour(eta_x, eta_y, dsf_new, f'increment', 'SF')
+    contour(eta_x, eta_y, dvp_new, f'increment', 'VP')
+
 if __name__ == '__main__':
     # Tikhonov's regularisation parameter
-    alpha = 9e-8
+    alpha = 0
     # time to plot
     time_index = 700
-    transform_plots(alpha, time_index)
+    conv = 'convergence'
+    transform_plots(alpha, time_index, conv)
 

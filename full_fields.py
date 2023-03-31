72ae19266d5ed0d6b7a95afc3297a2572354fee1
LauraRisley <l.risley@pgr.reading.ac.uk>
2023-03-31 08:13:30 +0100

    Function to run plots for all times, will then eventually make an animation.

--- full_fields.py
+++ full_fields.py
@@ -5,7 +5,7 @@ Functions to just plot the full fields of elevation and the horizontal velocity 
 from read_nemo_fields import *
 from general_functions import *
 
-def plots(time_index):
+def one_plot(time_index):
     """
     """
     # netcdf file locations
@@ -33,8 +33,39 @@ def plots(time_index):
     contour(u_lon, u_lat, u, f'at time {time[time_index]}', 'Zonal Velocity')
     contour(v_lon, v_lat, v, f'at time {time[time_index]}', 'Meridional Velocity')
 
+def plots_full():
+    """
+    """
+    # netcdf file locations
+    eta_input_file = "/c/Users/tk815965/OneDrive - University of Reading/Data_Assimilation/GYRE_config/instant.grid_T_depth0.nc"
+    u_input_file = "/c/Users/tk815965/OneDrive - University of Reading/Data_Assimilation/GYRE_config/instant.grid_U_depth0.nc"
+    v_input_file = "/c/Users/tk815965/OneDrive - University of Reading/Data_Assimilation/GYRE_config/instant.grid_V_depth0.nc"
+
+    # lon and lat for each grid
+    eta_lon, eta_lat, time = read_file_info(eta_input_file)
+    u_lon, u_lat, time = read_file_info(u_input_file)
+    v_lon, v_lat, time = read_file_info(v_input_file)
+
+    print(f'Length of elevation lattitude : {np.shape(eta_lat)} and longitude : {np.shape(eta_lon)}.')
+    print(f'Length of u lattitude : {np.shape(u_lat)} and longitude : {np.shape(u_lon)}.')
+    print(f'Length of v lattitude : {np.shape(v_lat)} and longitude : {np.shape(v_lon)}.')
+
+    num_times = np.size(time)
+    print(num_times)
+
+    for time_index in range(num_times - 1):
+        print(f'Plots at {time_index}')
+        eta = read_file(eta_input_file, "sossheig", time_index=time_index)
+        u = read_file(u_input_file, "vozocrtx", time_index=time_index)[0]
+        v = read_file(v_input_file, "vomecrty", time_index=time_index)[0]
+
+        contour(eta_lon, eta_lat, eta, f'time_{time[time_index]}', 'Elevation')
+        contour(u_lon, u_lat, u, f'time_{time[time_index]}', 'Zonal Velocity')
+        contour(v_lon, v_lat, v, f'time_{time[time_index]}', 'Meridional Velocity')
+
 if __name__ == '__main__':
     # time to plot
     time_index = 0
-    plots(time_index)
+    #one_plot(time_index)
+    plots_full()
 

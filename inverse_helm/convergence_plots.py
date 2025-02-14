f0922d13dfd76adccb86cfd8ea1a51b7abf993c9
LauraRisley <l.risley@pgr.reading.ac.uk>
2025-02-14 18:26:19 +0000

    Added a plot of the number of iterations across multiple depths.

--- inverse_helm/convergence_plots.py
+++ inverse_helm/convergence_plots.py
@@ -23,7 +23,30 @@ def conv_plot():
     plt.title(f'Convergence with accuracy tolerance = $10^{{{int(np.log10(1e-5))}}}$')
     plt.show()
 
+def iterations_across_depths_plot():
+    exps = [27, 21, 31, 35, 37]
+    label = [1e-3, 1e-4, 1e-5, 1e-6, 1e-7]
+    grays = np.linspace(0.2, 0.8, len(exps))  # Creates an array of grays from 0 (black) to 1 (white)
+    depths = np.loadtxt('/Users/tk815965/OneDrive - University of Reading/Data_Assimilation/GYRE_config/inverse_helm/gyre_depths.txt', delimiter=',')
+    print(len(depths))
+    # Create a dictionary to hold the arrays
+    arrays = {}
+    j=0
+    for i in exps:
+        i=int(i)
+        iters = np.loadtxt(f'/Users/tk815965/OneDrive - University of Reading/Data_Assimilation/GYRE_config/inverse_helm/convergence_files/iterations_{i}_alldepths.txt',delimiter=",")
+        print(f'Exp {i} = {len(iters)}')
+        plt.plot(iters, depths, color=str(grays[j]), label=rf"$10^{{{int(np.log10(label[j]))}}}$")
+        j+=1
+    plt.xlabel('Iterations')
+    plt.ylabel(f'Depth ($m$)')
+    plt.ylim(4600, -100)
+    #plt.yscale('log')
+    plt.legend(title = 'Regularisation parameter', loc=(0.4,0.4))
+    plt.title(f'Number of iterations with accuracy tolerance = $10^{{{int(np.log10(1e-5))}}}$')
+    plt.show()
 
 
 if __name__ == '__main__':
-    conv_plot()
+    #conv_plot()
+    iterations_across_depths_plot()
